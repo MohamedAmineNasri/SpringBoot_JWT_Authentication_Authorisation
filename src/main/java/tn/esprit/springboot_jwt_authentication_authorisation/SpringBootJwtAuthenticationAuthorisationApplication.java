808a768ec0af983b8cc080c1bca0c1b7df7715dd
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-11-21 20:59:22 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/springboot_jwt_authentication_authorisation/SpringBootJwtAuthenticationAuthorisationApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.springboot_jwt_authentication_authorisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthenticationAuthorisationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtAuthenticationAuthorisationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/springboot_jwt_authentication_authorisation/SpringBootJwtAuthenticationAuthorisationApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.springboot_jwt_authentication_authorisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthenticationAuthorisationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
