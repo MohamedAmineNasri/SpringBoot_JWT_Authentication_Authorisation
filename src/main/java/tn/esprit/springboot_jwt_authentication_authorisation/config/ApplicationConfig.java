34d76f526dac74ddce8d462b86734d04362bf6ab
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-11-22 16:03:02 +0100

    Implement the UserDetailsService

---
+++ src/main/java/tn/esprit/springboot_jwt_authentication_authorisation/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package tn.esprit.springboot_jwt_authentication_authorisation.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import tn.esprit.springboot_jwt_authentication_authorisation.repositories.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("User Not Found"));
+    }
+}
