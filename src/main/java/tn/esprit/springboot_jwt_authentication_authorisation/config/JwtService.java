86f16fd6f2e7fa0a0b3a1bacc923499c619e3d05
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-11-22 12:58:02 +0100

    Checking/CreateJWT(token/service)/Add(JJWT dependencies)

---
+++ src/main/java/tn/esprit/springboot_jwt_authentication_authorisation/config/JwtService.java
@@ -0,0 +1,10 @@
+package tn.esprit.springboot_jwt_authentication_authorisation.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+}
