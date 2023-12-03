067d30eeafdc9348e1d924406e4ba80c6db84dfe
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-12-03 13:28:40 +0100

    implement the logout handler service

--- src/main/java/tn/esprit/springboot_jwt_authentication_authorisation/config/LogoutService.java
+++ src/main/java/tn/esprit/springboot_jwt_authentication_authorisation/config/LogoutService.java
@@ -6,12 +6,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
+import tn.esprit.springboot_jwt_authentication_authorisation.repositories.TokenRepository;
 
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            return;
+        }
+        jwt = authHeader.substring(7);
+        var storedToken = tokenRepository.findByToken(jwt).orElse(null);
+        if (storedToken !=null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+        }
     }
 }
